3e71aa0e3fc5920e33be689a7309316f8bd4d64b
Daishi Kato <dai-shi@users.noreply.github.com>
2018-04-08 10:27:47 +0900

    Update fetcher-v2.js

--- user-scripts/fetcher-v2.js
+++ user-scripts/fetcher-v2.js
@@ -1,1 +1,20 @@
 location.href='https://assistant.google.com/explore/g/5?hl=ja';
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const scrollDown = async () => {
+  let lastPos = 0;
+  let pos = -1;
+  while (pos !== lastPos) {
+    document.getElementsByClassName('y3IDJd')[0].scrollTop = 999999999;
+    await sleep(100);
+    lastPos = pos;
+    pos = document.getElementsByClassName('y3IDJd')[0].scrollTop;
+  }
+};
+const getData = () => JSON.stringify([].map.call(document.querySelectorAll('.GpVDee'), x => ({ id: x.getAttribute('href'), name: x.querySelector('.hzMTRd').innerHTML, author: x.querySelector('.OlFmif').innerHTML, description: x.querySelector('.fOsD6b').innerHTML })), null, 2);
+const main = async () => {
+  await scrollDown();
+  console.log(getData());
+};
+
+main();
