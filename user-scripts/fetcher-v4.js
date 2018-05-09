d26b1e25022d378c2ab0b17c10856890b789e5bf
Daishi Kato <dai-shi@users.noreply.github.com>
2018-05-09 18:21:15 +0900

    Update fetcher-v4.js

--- user-scripts/fetcher-v4.js
+++ user-scripts/fetcher-v4.js
@@ -11,21 +11,18 @@ const scrollDown = async () => {
   }
 };
 const getFirstText = ele => ele.firstChild && ele.firstChild.nodeType === Node.TEXT_NODE && ele.firstChild.textContent;
-const getLink = ele => ele && ele.getAttribute('href');
-const getEmail = ele => { const href = getLink(ele); return href && href.startsWith('mailto:') && href.slice(7); };
 const getItem = () => ({
-  name: document.querySelector('.nHPWwd').innerHTML,
-  author: getFirstText(document.querySelector('.Ry0mub')) || '',
+  name: document.querySelector('.XIPoR.RfR9R').innerHTML,
+  author: getFirstText(document.querySelector('.GCKci.CbqDob')) || '',
   description: document.querySelector('.JqJiee').innerHTML,
-  email: getEmail(document.querySelector('.ReVo3:nth-of-type(1)')),
-  privacy: getLink(document.querySelector('.ReVo3:nth-of-type(2)')),
+  email: document.querySelector('.UxXfse.CdFZQ').innerHTML,
+  privacy: document.querySelector('.uyYuVb.oJeWuf').getAttribute('data-link'),
 });
 const getData = async () => {
   const elements = [].filter.call(document.querySelectorAll('a'), ele => ele.getAttribute('href').startsWith('services/'));
   const items = [];
   for (let i = 0; i < elements.length; i += 1) {
     const ele = elements[i];
-    // if (ele.getAttribute('href') === 'services/a/uid/0000000cda5d5b9f?hl=ja') continue; // 404
     ele.click();
     let item = null;
     while(!item) {
